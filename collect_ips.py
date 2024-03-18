10b3870311c772d015b4fa0c6923e420d121617e
iamfang2019 <56925364+iamfang2019@users.noreply.github.com>
2024-03-18 11:17:36 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -1,12 +1,11 @@
+import os
+import re
 import requests
 from bs4 import BeautifulSoup
-import re
-import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz'
-        ]
+        'https://ip.164746.xyz']
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
@@ -15,7 +14,7 @@ ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 if os.path.exists('ip.txt'):
     os.remove('ip.txt')
 
-# 创建一个文件来存储IP地址
+# 创建一个文件来存储响应时间小于300毫秒的IP地址
 with open('ip.txt', 'w') as file:
     for url in urls:
         # 发送HTTP请求获取网页内容
@@ -37,8 +36,16 @@ with open('ip.txt', 'w') as file:
             element_text = element.get_text()
             ip_matches = re.findall(ip_pattern, element_text)
             
-            # 如果找到IP地址,则写入文件
+            # 如果找到IP地址,则进行ping测试
             for ip in ip_matches:
-                file.write(ip + ':8443#CF''\n')
+                # 执行ping命令
+                response = os.popen(f'ping -n 1 {ip}').read()
+                
+                # 使用正则表达式匹配ping响应时间
+                ping_time = re.findall(r'时间=(\d+)ms', response)
+                
+                # 如果ping响应时间存在且小于300毫秒，则保存IP地址
+                if ping_time and int(ping_time[0]) < 300:
+                    file.write(ip +  ':8443#CF''\n')
 
-print('IP地址已保存到ip.txt文件中。')
+print('响应时间小于300毫秒的IP地址已保存到ip.txt文件中。')
